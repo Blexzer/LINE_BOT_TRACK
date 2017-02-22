ddbf80a7b76ef849609a71f55ea67185348f0578
Blexzer <beam_coe@yahoo.com>
2017-02-22 21:06:44 +0700

    Edit Repo

--- repo.php
+++ repo.php
@@ -63,65 +63,104 @@ $events = json_decode($content, true);
 // 		}
 // 	}
 // }
-if (!empty($events['events'])) {
-    // Loop through each event
-	foreach ($events['events'] as $event) {
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			$text = $event['message']['text'];
-			$replyToken = $event['replyToken'];
-			$strCut = explode('RDC',strtoupper($text));
-			for($i=0;$i<sizeof($strCut);$i++){
-				if(strlen($strCut[$i])>=10) {
-					$countStr = str_split($strCut[$i], 10);
-					if(is_numeric($countStr[0])){
-						$barcode = 'RDC'.$countStr[0];
-						$getBarcode[] = $barcode;
-					}
-				}
-			}
+// if (!empty($events['events'])) {
+//     // Loop through each event
+// 	foreach ($events['events'] as $event) {
+// 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+// 			$text = $event['message']['text'];
+// 			$replyToken = $event['replyToken'];
+// 			$strCut = explode('RDC',strtoupper($text));
+// 			for($i=0;$i<sizeof($strCut);$i++){
+// 				if(strlen($strCut[$i])>=10) {
+// 					$countStr = str_split($strCut[$i], 10);
+// 					if(is_numeric($countStr[0])){
+// 						$barcode = 'RDC'.$countStr[0];
+// 						$getBarcode[] = $barcode;
+// 					}
+// 				}
+// 			}
 
-			// if(sizeof($getBarcode)>0){
-			// 	for($i= 0 ;$i<sizeof($getBarcode);$i++){
-			// 		$lineSession = 'testBot';
+// 			// if(sizeof($getBarcode)>0){
+// 			// 	for($i= 0 ;$i<sizeof($getBarcode);$i++){
+// 			// 		$lineSession = 'testBot';
 
-			// 		$dataX = array("DATA" => $getBarcode[$i], "CREATE" => $lineSession);
-			// 		$data_string = json_encode($dataX);
-			// 		$urlBWAPI = "http://122.155.180.139/SERVICETRACK/service_linebot_track_temp.php" ;
-			// 		$resultApi[] = json_decode(postJSONdataAPI($urlBWAPI, $data_string),true);
-			// 	}
-			// }
-			if(sizeof($getBarcode)>0){
-				for($i= 0 ;$i<sizeof($getBarcode);$i++){
-					$messages[]  = array('type' => 'text','text' => $getBarcode[$i]);
-					// 'type' => 'text',
-					// 'text' => $getBarcode[$i]
-					// ;
-			   // Make a POST Request to Messaging API to reply to sender
+// 			// 		$dataX = array("DATA" => $getBarcode[$i], "CREATE" => $lineSession);
+// 			// 		$data_string = json_encode($dataX);
+// 			// 		$urlBWAPI = "http://122.155.180.139/SERVICETRACK/service_linebot_track_temp.php" ;
+// 			// 		$resultApi[] = json_decode(postJSONdataAPI($urlBWAPI, $data_string),true);
+// 			// 	}
+// 			// }
+// 			if(sizeof($getBarcode)>0){
+// 				for($i= 0 ;$i<sizeof($getBarcode);$i++){
+// 					$messages[]  = array('type' => 'text','text' => $getBarcode[$i]);
+// 					// 'type' => 'text',
+// 					// 'text' => $getBarcode[$i]
+// 					// ;
+// 			   // Make a POST Request to Messaging API to reply to sender
 					
-				}
-					$url = 'https://api.line.me/v2/bot/message/reply';
-					$data = [
-					'replyToken' => $replyToken,
-					'messages' => [$messages],
-					];
-					$post = json_encode($data);
-					$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+// 				}
+// 					$url = 'https://api.line.me/v2/bot/message/reply';
+// 					$data = [
+// 					'replyToken' => $replyToken,
+// 					'messages' => [$messages],
+// 					];
+// 					$post = json_encode($data);
+// 					$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-					$ch = curl_init($url);
-					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-					$result = curl_exec($ch);
-					curl_close($ch);
+// 					$ch = curl_init($url);
+// 					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+// 					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// 					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+// 					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// 					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// 					$result = curl_exec($ch);
+// 					curl_close($ch);
 
-					echo json_encode($messages);
-			}
+// 					echo json_encode($messages);
+// 			}
 
-		}
-	}
+// 		}
+// 	}
 
+// }
+
+if (!is_null($events['events'])) {
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+        // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get text sent
+            $text = $event['message']['text'];
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+
+            // Build message to reply back
+            $messages = array(
+                'type' => 'text',
+                'text' => $text
+            );
+
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            echo $result . "\r\n";
+        }
+    }
 }
 echo "OK";
 
